195003cbc672a70750e0781f91e2fa828ff6b310
Suzan Letjobana Masite <suzan@istratgo.com>
2024-06-29 21:14:05 +0200

    Added day 1 solution

---
+++ AdventOfCode2024/Days/Day1/Day1.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2024.Days.Day1
+{
+    public class Day1
+    {
+        public static void Part1()
+        {
+            var lines = File.ReadAllLines("Days/Day1/input.txt");
+            int sum = lines.Sum(line => ExtractCalibrationValue(line));
+            Console.WriteLine("Sum of all calibration values: " + sum);
+        }
+
+        private static int ExtractCalibrationValue(string line)
+        {
+            char firstDigit = line.FirstOrDefault(char.IsDigit);
+            char lastDigit = line.LastOrDefault(char.IsDigit);
+            string twoDigitNumberStr = firstDigit.ToString() + lastDigit.ToString();
+            return int.Parse(twoDigitNumberStr);
+        }
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode2024.Days.Day1;
+
+namespace AdventOfCode2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.Part1();
+        }
+    }
+}
